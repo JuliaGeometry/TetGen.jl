2cad33802d3425580c31c6a3281ee5a12dadb195
SimonDanisch <sdanisch@gmail.com>
2017-11-13 16:46:57 +0100

    first steps

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,11 @@ using Base.Test
 
 # write your own tests here
 @test 1 == 1
+
+tetgen = "./" * Pkg.dir("TetGen", "deps", "tetgen")
+cd(Pkg.dir("TetGen", "deps"))
+example = readstring("example.poly")
+
+open(`./tetgen -p `, "w") do f
+    print(f, example)
+end
