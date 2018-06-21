468934dba6467cd987ae053ff883ba0858321bae
SimonDanisch <sdanisch@gmail.com>
2018-06-21 12:23:49 +0200

    merge

--- src/TetGen.jl
+++ src/TetGen.jl
@@ -1,4 +1,11 @@
 module TetGen
 
+const depsfile = joinpath(@__DIR__, "..", "deps", "deps.jl")
+
+if isfile(depsfile)
+    include(depsfile)
+else
+    error("Tetgen not build correctly. Please run Pkg.build(\"TetGen\")")
+end
 
 end # module
