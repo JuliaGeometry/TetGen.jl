00ea1bbff61e570098740290457b8ab8831528c5
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2020-12-22 16:34:33 +0100

    give up the __init__ check and relay on Pkg => codecov should be ok then

--- src/TetGen.jl
+++ src/TetGen.jl
@@ -1,21 +1,6 @@
 module TetGen
 using DocStringExtensions
 using TetGen_jll
-using Libdl
-
-
-# Do this here as we appearantly cannot pin build numbers in [compat}
-# At least we provide an error message here if this fails.
-function __init__()
-    lib=something(Libdl.dlopen_e(libtet,Libdl.RTLD_NOW),C_NULL)
-    try
-        sym = Libdl.dlsym(lib, :tetrahedralize2_f64)  
-    catch err
-        println(err)
-        println("Ensure updating TetGen_jll to version 1.5.1+1")
-        rethrow(err)
-    end
-end
 
 using GeometryBasics
 using GeometryBasics: Polygon, MultiPolygon, Point, LineFace, Polytope, Line,
